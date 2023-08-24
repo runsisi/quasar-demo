3dc9950f30640bcfd43147be809617de74d3e536
runsisi <runsisi@hust.edu.cn>
2023-08-24 10:20:13 +0800

    Create gh-pages.yml
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- quasar.config.js
+++ quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: 'quasar-demo',
       // analyze: true,
       // env: {},
       // rawDefine: {}
