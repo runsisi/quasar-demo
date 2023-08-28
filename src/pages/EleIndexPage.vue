fe30a15e3fc1f905081930963b115e466ad06c3c
runsisi <runsisi@hust.edu.cn>
2023-08-28 16:59:07 +0800

    provide/inject axios
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- src/pages/EleIndexPage.vue
+++ src/pages/EleIndexPage.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 
+import {inject} from "vue";
+import {AxiosInstance} from "axios";
+
+const api = inject('api') as AxiosInstance
+
+console.log(api)
+
+api.get('/test')
+
 </script>
 
 <template>
